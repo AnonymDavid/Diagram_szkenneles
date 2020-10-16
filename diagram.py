793e2a047e721a48810a651a6c74f272b353b88a
AnonymDavid <jodavid512@gmail.com>
2020-10-16 15:46:49 +0200

    Improvements

--- diagram.py
+++ diagram.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2
 
 # read, make it black and white
-img = cv2.imread("tests/test_3_1.jpg")
+img = cv2.imread("tests/test2.jpg")
+
+kernel = np.ones((3,3), np.uint8)
+
 # edges
 canny = cv2.Canny(img, 200, 255)
 
@@ -19,8 +22,6 @@ _, _, mask, _ = cv2.floodFill(canny, mask, (0,0), (255,0,0), (10,)*3, (10,)*3, f
 
 # erode preparations
 mask = 255-mask
-
-kernel = np.ones((3,3), np.uint8)
 
 # erode until the arrows disappear (contour count doesn't change after erosion)
 
@@ -41,23 +42,28 @@ while prevCntLen != len(contours):
 #dilate = cv2.dilate(erode, kernel, erodeIterations)
 dilate = erode
 for i in range(erodeIterations):
-    dilate = cv2.dilate(dilate, kernel, erodeIterations)
+    dilate = cv2.dilate(dilate, kernel)
 
 contours, _ = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 for cnt in contours:
     peri = cv2.arcLength(cnt, True)
-    approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+    approx = cv2.approxPolyDP(cnt, 0.025 * peri, True)
     
     x, y, w, h = cv2.boundingRect(approx)
     cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,255), 3)
     
     cv2.putText(img, str(len(approx)), (x,y+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
 
-#arrows = cv2.subtract(mask, dilate)
+for i in range(erodeIterations):
+    dilate = cv2.dilate(dilate,kernel)
+    
+arrows = cv2.subtract(mask, dilate)
 
-#cv2.imshow("canny", canny)
-#cv2.imshow("mask", mask)
+cv2.imshow("dilate", dilate)
+cv2.imshow("mask", mask)
+
+cv2.imshow("arrows", arrows)
 
 cv2.imshow("img", img)
 cv2.waitKey()
